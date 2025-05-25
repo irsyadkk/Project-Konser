907dc5710049fdc61eff451ddd3d016ce8aa0985
irsyadkk <irsyadkhairullah7@gmail.com>
2025-05-25 18:16:47 +0700

    fix logic

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,19 +1,24 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
 
-const Admin = db.define('admin',{
+const Admin = db.define(
+  "admin",
+  {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     nama: Sequelize.STRING,
     email: Sequelize.STRING,
+    umur: Sequelize.INTEGER,
     pass: Sequelize.STRING,
-    refresh_token: Sequelize.TEXT
-    },{
-    freezeTableName:true
-    });
+    refresh_token: Sequelize.TEXT,
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
 db.sync().then(() => console.log("Database synced"));
 
