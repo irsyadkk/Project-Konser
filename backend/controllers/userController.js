23d62658d24c023c38d35519724a5c6954fb1c06
irsyadkk <irsyadkhairullah7@gmail.com>
2025-05-27 15:56:21 +0700

    fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import { Error } from "sequelize";
-import User from "../models/userModel";
+import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
