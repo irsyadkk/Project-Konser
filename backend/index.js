05e80788f94675c559abb51b2ab828fa9d41e297
irsyadkk <irsyadkhairullah7@gmail.com>
2025-05-28 01:04:52 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,22 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
-const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS;
+const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(",") || [];
 const app = express();
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || ALLOWED_ORIGINS.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS: Not allowed origin -> " + origin));
+    }
+  },
+  credentials: true,
+};
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: ALLOWED_ORIGINS }));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.get("/", (req, res) => res.render("index"));
 app.use(route);
